cc97ec80c98dd9488692bd9a23b7b0ea17bb79fa
Nihal1203 <nihalcharola11@gmail.com>
2024-06-13 16:43:43 +0530

    update in connection to database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
 const mongoose=require('mongoose')
 module.exports=()=>{
   
-  try {
-    mongoose.connect(process.env.DB)
-    console.log("Connected To Database")
-  } catch (error) {
-    console.log("Error while connecting to database",error)
-  }
-
+  
+   mongoose.connect(process.env.DB)
+  .then(() => console.log('Connected to MongoDB Database'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+    
+  
 }
